02faf623910a1b499a0633463526cacd1a1de069
estherjoanne <esther_joanna_chin@students.edu.sg>
2025-04-11 06:43:25 +0000

    Add bkgrnd, ball, paddles

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -9,9 +9,16 @@ export class Game extends Scene {
     }
 
     preload() {
+        this.load.image("background", "assets/background.png");
+        this.load.image("ball", "assets/ball.png");
+        this.load.image("paddle", "assets/paddle.png");
     }
 
     create() {
+        this.add.image(WIDTH/2, HEIGHT/2, "background").setScale(0.8, 0.8);
+        this.ball = this.add.image(WIDTH/2, HEIGHT/2, "ball").setScale(0.1,0.1);
+        this.leftPaddle = this.add.image(50,384, "paddle")
+        this.rightPaddle = this.add.image(974,384, "paddle")
     }
 
     update() {
